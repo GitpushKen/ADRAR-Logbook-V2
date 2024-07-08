248a82b2d90543ed7b42d717e97e06f0a9d1f62c
Ken <glxtchedface@outlook.fr>
2024-07-08 12:23:39 +0200

    pkmn4

--- sous-dossier/API Fetch/pokemon.js
+++ sous-dossier/API Fetch/pokemon.js
@@ -155,13 +155,13 @@ const pokeBattle = () => {
       let randomPokemon = Math.floor(Math.random() * 386);
 
       const poke = await fetch(
-        `https://pokeapi.firebaseapp.com/api/v2/pokemon/${randomPokemon}/`
+        `http://pokeapi.co/api/v2/pokemon/${randomPokemon}/`
       );
       const data = await poke.json();
       console.log(data);
 
       const pokeLegend = await fetch(
-        `https://pokeapi.firebaseapp.com/api/v2/pokemon-species/${randomPokemon}/`
+        `http://pokeapi.co/api/v2/pokemon-species/${randomPokemon}/`
       );
       const dataLegend = await pokeLegend.json();
       console.log(dataLegend);
